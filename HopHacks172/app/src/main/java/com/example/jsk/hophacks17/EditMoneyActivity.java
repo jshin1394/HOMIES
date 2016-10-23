abdb4fbaf1fdb522198402652a76c5dcc0b3cf16
Joo Chang Lee <jamesleechc@hotmail.com>
2016-10-22 23:16:16 -0400

    working on edit money

--- HopHacks172/app/src/main/java/com/example/jsk/hophacks17/EditMoneyActivity.java
+++ HopHacks172/app/src/main/java/com/example/jsk/hophacks17/EditMoneyActivity.java
@@ -1,10 +1,47 @@
 package com.example.jsk.hophacks17;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
 
 /**
  * Created by James on 2016-10-22.
  */
 
 public class EditMoneyActivity extends AppCompatActivity {
+
+    private String userName;
+    private int houseID;
+    private List<Member> members;
+    private List<Double> amounts;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_money);
+
+        //Get information from login page.
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if (extras == null) {
+                userName = null;
+                houseID = 0;
+                members = null;
+            } else {
+                userName = extras.getString("userName");
+                houseID = extras.getInt("houseID");
+                members = (List<Member>) extras.getSerializable("members");
+            }
+        } else {
+            userName = savedInstanceState.getString("userName");
+            houseID = savedInstanceState.getInt("houseID");
+            members = (List<Member>) savedInstanceState.getSerializable("members");
+        }
+    }
+
+
+
+
 }
